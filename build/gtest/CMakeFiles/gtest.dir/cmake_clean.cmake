5cbe2972a8473ad0c268b66b8f3413acf26fab61
Fredrik Ryden <fredrik.ryden1@gmail.com>
2014-04-24 18:34:27 -0700

    Commit all

---
+++ build/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/pierre/Dropbox/uh/uh1/ros_ws/rviz_plugin_template/lib/libgtest.pdb"
+  "/home/pierre/Dropbox/uh/uh1/ros_ws/rviz_plugin_template/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
